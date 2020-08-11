c1e091bfd231dc7c5bed403da2493cfd1d4e1fb3
Nikolay <XevtinX@gmail.com>
2020-08-11 13:08:02 +0300

    Start Gradle project
    Just create Gradle project on Spring Boot

---
+++ cvbot/src/main/java/com/xevtinx/cvbot/CvbotApplication.java
@@ -0,0 +1,13 @@
+package com.xevtinx.cvbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvbotApplication.class, args);
+	}
+
+}
---
+++ cvbot/src/test/java/com/xevtinx/cvbot/CvbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xevtinx.cvbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
